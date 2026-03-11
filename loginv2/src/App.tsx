a09763108ec8939c11aeedcee5b3e8aa83c7ecb5
Tehrence Llenarez <121592180+teejx@users.noreply.github.com>
2026-03-11 13:28:03 +0800

    Initial commit

---
+++ loginv2/src/App.tsx
@@ -0,0 +1,61 @@
+function App() {
+  return (
+    <div className="flex min-h-screen">
+      <div className="w-3/1 hidden md:inline-block">
+        <img
+          src="/src/assets/login-bg.png"
+          className="h-full w-full object-cover"
+        />
+      </div>
+
+      <div className="w-full flex flex-col items-center justify-center">
+        <form className="md:w-96 w-80 flex flex-col items-center justify-center ">
+          <div className="flex flex-col text-left w-full pb-6">
+            <h1 className="text-[16px] font-semibold">Welcome to the</h1>
+            <h2 className="text-[30px] font-black bg-linear-to-t from-[#2600FF] to-[#FF8400] to-60% bg-clip-text text-transparent">
+              Digital Ecosystem
+            </h2>
+          </div>
+
+          <div className="flex items-center w-full bg-transparent border border-gray-300/60 h-12 rounded-full overflow-hidden pl-6 gap-2">
+            <input
+              type="email"
+              placeholder="Enter your email"
+              className="bg-transparent text-gray-500/80 placeholder-gray-500/80 outline-none text-sm w-full h-full"
+              required
+            />
+          </div>
+
+          <div className="flex items-center mt-6 w-full bg-transparent border border-gray-300/60 h-12 rounded-full overflow-hidden pl-6 gap-2">
+            <input
+              type="password"
+              placeholder="Password"
+              className="bg-transparent text-gray-500/80 placeholder-gray-500/80 outline-none text-sm w-full h-full"
+              required
+            />
+          </div>
+
+          <div className="w-full flex items-center justify-between mt-8 text-gray-500/80">
+            <div className="flex items-center gap-2">
+              <input className="h-5" type="checkbox" id="checkbox" />
+              <label className="text-sm" htmlFor="checkbox">
+                Remember me
+              </label>
+            </div>
+            <a className="text-sm underline" href="#">
+              Forgot password?
+            </a>
+          </div>
+
+          <button
+            type="submit"
+            className="mt-8 w-full h-11 rounded-full text-white bg-indigo-500 hover:opacity-90 transition-opacity"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+export default App;
